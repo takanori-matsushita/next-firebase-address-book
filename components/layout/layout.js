f210e370a90417d02b553defa82eb5df6553eceb
takanori-matsushita <t.matsushita@arcjobz.com>
2021-10-06 21:56:56 +0900

    [add]layout component

---
+++ components/layout/layout.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default function Layout(props) {
+  return (
+    <>
+      <Head>
+        <title>{props.title}</title>
+        <meta name="description" content={props.description} />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+          crossOrigin="anonymous"
+        ></link>
+      </Head>
+      <Header />
+      <div className="container">{props.children}</div>
+      <Footer />
+    </>
+  );
+}
